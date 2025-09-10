8270ac79888f2fead5e69cf3ce47b481ca9d06f4
Sasi349 <sasidharan.developer@gmail.com>
2025-09-10 05:33:35 +0530

    Setup Light & dark Theme, Shadcn and NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 const Home = () => {
   return (
     <>
       <h1 className="h1-bold">Tailwindcss is fun</h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the world of nextjs
-      </h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 };
